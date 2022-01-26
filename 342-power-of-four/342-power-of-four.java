b80373ce4758cfd731d4cdf514b9f812ab61c86d
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-01-26 23:36:07 +0530

    Time: 2 ms (44.93%), Space: 38.1 MB (26.18%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-       int digit=(int)(Math.log(n)/Math.log(2))+1;
-       if(n<=0) return false;
-       if ((n & (n-1))==0&& digit%2==1){
-         return true;
-     }
-        return false;
+        return (Math.log(n)/Math.log(4))%1==0; 
     }
 }
