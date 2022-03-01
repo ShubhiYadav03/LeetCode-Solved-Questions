e801b01d4b4adfe2261b6efd8e4752042964b5eb
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-03-01 22:14:00 +0530

    Time: 1 ms (99.97%), Space: 48.2 MB (24.49%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -4,9 +4,7 @@ class Solution {
         if(n==1) return new int[]{0,1};
         
         int[] arr=new int[n+1];
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2;i<n+1;i++){
+        for(int i=1;i<n+1;i++){
             arr[i] = arr[i>>1] + i%2;
         }
         return arr;
