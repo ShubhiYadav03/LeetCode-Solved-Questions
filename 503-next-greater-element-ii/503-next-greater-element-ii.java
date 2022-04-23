02c50e6c12c316e9cf392d602ecc0cd8054963fd
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-04-24 03:23:55 +0530

    Time: 38 ms (55.71%), Space: 54.3 MB (71.85%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.java
+++ 503-next-greater-element-ii/503-next-greater-element-ii.java
@@ -3,29 +3,27 @@ class Solution {
         Stack<Integer> st=new Stack<>();
         int[] ans=new int[nums.length];
         st.push(0);
-        for(int i=1;i<nums.length;i++){
-            while(st.size()>0&&nums[st.peek()]<nums[i]){
-                ans[st.peek()]=nums[i];
+        for(int i=1;i<nums.length*2;i++){
+            while(st.size()>0&&nums[st.peek()]<nums[i%nums.length]){
+                ans[st.peek()]=nums[i%nums.length];
                 st.pop();
                 
             }
-            st.push(i);
-            
+            if(i<nums.length) st.push(i);         
         }
-        if(!st.isEmpty()){
-            for(int i=0;i<=st.peek();i++){
-            while(st.size()>0&&nums[st.peek()]<nums[i]){
-                ans[st.peek()]=nums[i];
-                st.pop();              
-            }
-        }
+        // if(!st.isEmpty()){
+        //     for(int i=0;i<=st.peek();i++){
+        //     while(st.size()>0&&nums[st.peek()]<nums[i]){
+        //         ans[st.peek()]=nums[i];
+        //         st.pop();              
+        //     }
+        // }
             while(st.size()>0){
                 ans[st.peek()]=-1;
                 st.pop();              
             }
-            
+             return ans; 
     }
         
-        return ans;
-}
+      
 }
