99f4e8e132bb61a398ca7e51ac9d9928ee0818da
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-02 23:12:48 +0530

    Time: 1 ms (76.94%), Space: 43.6 MB (22.62%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        
+      //  double ans=1;
+        int p=n<0?-1*n:n;
+        
+        
+        double ans=pow(x,p);
+        return (n>0)?ans:1/ans;
+        
+        
+    }
+    public double pow(double x, int n) {
+        if (n ==0) return 1;
+        if(x==0) return 0;
+        //if(n==1) return x;
+        double res=pow(x,n/2);
+        res=res*res;
+        return (n%2==0)? res:res*x;
+        
+        
     }
 }
