bf459e88ebd3e6a92a7b8e5d41f5e77c8088e726
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-02 23:49:25 +0530

    Time: 1 ms (42.06%), Space: 41.5 MB (13.80%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,7 +5,6 @@ class Solution {
             for(int j=n-1;j>=0;j--){
                 if(i==m-1||j==n-1) {
                     dp[i][j]=1;
-                    continue;
                 }
                 else{
                     dp[i][j]=dp[i+1][j]+dp[i][j+1];
