cd462582e0d7daa3ffce53f9fe6480ea5b3b2510
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-07 21:30:43 +0530

    Time: 1 ms (70.69%), Space: 43.4 MB (21.01%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -17,14 +17,14 @@ class Solution {
                 size++;
                 temp=temp.next;            
             }
-        System.out.println(temp.val);
+       // System.out.println(temp.val);
         temp.next=head;
         k=k%(size+1);       
             ListNode temp2=head, temp3=null;
             for(int i=1;i<size-k+1;i++){
                 temp2=temp2.next;     
             }
-        System.out.println(temp2.val);
+       // System.out.println(temp2.val);
        // System.out.println(temp3.val);
             temp3=temp2.next;
             
