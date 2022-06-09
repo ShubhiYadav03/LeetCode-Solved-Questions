9ea2c3d281f93a647a004c062ee6dfb75f11c6f9
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-09 19:28:02 +0530

    Time: 9 ms (14.01%), Space: 42.5 MB (83.67%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,24 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st=new Stack<>();
-        int[] ans=new int[nums2.length];
         int[] res=new int[nums1.length];
-        st.push(nums2[nums2.length-1]);
-        ans[nums2.length-1]=-1;
-        for(int j=nums2.length-2;j>=0;j--){
-            while(!st.isEmpty()&&st.peek()<=nums2[j]) st.pop();
-            if(st.isEmpty()) ans[j]=-1;
-            else ans[j]=st.peek();
-            
-            st.push(nums2[j]);
+        int[] ans=new int[nums2.length];
+        st.push(0);
+        for(int i=1;i<nums2.length;i++){
+            while(!st.isEmpty() && nums2[i]>nums2[st.peek()]){
+                ans[st.pop()]=nums2[i];
+            }
+            st.push(i);
+        }
+        
+        while(!st.isEmpty()){
+            ans[st.pop()]=-1;
         }
         
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j])
-                    res[i]=ans[j];
+           for(int j=0;j<nums2.length;j++){
+            if(nums1[i]==nums2[j]){
+                res[i]=ans[j];
             }
+           } 
         }
+        
         return res;
     }
 }
