9e2d9a30c7cc017693c8489c77be3288717af174
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-09 23:26:07 +0530

    Time: 256 ms (13.88%), Space: 132.4 MB (33.48%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] dailyTemperatures(int[] temps) {
         Stack<Integer> st=new Stack<>();
         int[] ans=new int[temps.length];
-        Arrays.fill(ans,0);
+      //  Arrays.fill(ans,0);
         int count=0;
         for(int i=0;i<temps.length;i++){
             while(!st.isEmpty()&& temps[st.peek()]<temps[i]){
