8bdddb73908d7442bf8976712b1e57724020f710
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-10 22:17:55 +0530

    Time: 182 ms (50.28%), Space: 93.2 MB (31.93%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -7,36 +7,39 @@ class Solution {
         
         rb[heigths.length-1]=heigths.length;
         for(int i=1;i<heigths.length;i++){
-            while(st.size()>0&&heigths[i]<heigths[st.peek()]){
-                rb[st.peek()]=i;
-                st.pop();
+            while(st.size()>0&&heigths[i]<heigths[st.peek()]){    
+                int height=heigths[st.pop()];
+                int left=(!st.isEmpty()) ? st.peek() : -1;
+                area=Math.max(area,(i-left-1)*height);
             }
-          
+
             st.push(i);
             }
         while(st.size()>0){
-            rb[st.peek()]=heigths.length;
-            st.pop();
+            int right=heigths.length;
+            int height=heigths[st.pop()];
+                int left=(!st.isEmpty()) ? st.peek() : -1;
+                area=Math.max(area,(right-left-1)*height);
         } 
         
         st=new Stack<>();
         st.push(heigths.length-1);
-        int[] lb=new int[heigths.length];
-        lb[0]=-1;
-        for(int i=heigths.length-2;i>=0;i--){
-            while(st.size()>0&&heigths[i]<heigths[st.peek()]){
-                lb[st.peek()]=i;
-                st.pop();
-            }
-            st.push(i);
-            }
-        while(st.size()>0){
-            lb[st.peek()]=-1;
-            st.pop();
-        } 
-        for(int i=0;i<heigths.length;i++){
-            area=Math.max(area,(rb[i]-lb[i]-1)*heigths[i]);
-        }
+        // int[] lb=new int[heigths.length];
+        // lb[0]=-1;
+        // for(int i=heigths.length-2;i>=0;i--){
+        //     while(st.size()>0&&heigths[i]<heigths[st.peek()]){
+        //         lb[st.peek()]=i;
+        //         st.pop();
+        //     }
+        //     st.push(i);
+        //     }
+        // while(st.size()>0){
+        //     lb[st.peek()]=-1;
+        //     st.pop();
+        // } 
+//         for(int i=0;i<heigths.length;i++){
+           
+//         }
         return area;
     }
 }
