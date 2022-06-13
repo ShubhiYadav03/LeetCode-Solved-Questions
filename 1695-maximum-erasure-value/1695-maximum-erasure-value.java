75f3f9204fca5123d3094df74efe771ba42a2333
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-13 19:39:41 +0530

    Time: 61 ms (84.55%), Space: 55.1 MB (89.82%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,11 +3,10 @@ class Solution {
         HashSet<Integer> hs=new HashSet();
         int sum=0,maxSum=0,l=0;
         for(int i=0;i<nums.length;i++){
-            if(hs.contains(nums[i])){
-                  while(hs.contains(nums[i])) {
-                      sum-=nums[l];
-                     hs.remove(nums[l++]);                  
-            }}
+            while(hs.contains(nums[i])) {
+                sum-=nums[l];
+                hs.remove(nums[l++]);                  
+            }
             hs.add(nums[i]);
             sum+=nums[i];
             maxSum=Math.max(sum,maxSum);
