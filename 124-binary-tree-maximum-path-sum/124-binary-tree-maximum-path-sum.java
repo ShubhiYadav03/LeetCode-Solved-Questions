3213bfe4feef426f9246fb370e5c73e361c2f8b3
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-14 19:41:32 +0530

    Time: 2 ms (15.55%), Space: 47.8 MB (41.62%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -22,11 +22,12 @@ class Solution {
     int maxSum(TreeNode node){
         if(node==null) return 0;
         
-        int left=Math.max(0,maxSum(node.left));
-        int right = Math.max(0,maxSum(node.right));
+        int left=maxSum(node.left);
+        int right = maxSum(node.right);
         
-        max=Math.max(max,left+right+node.val);
+        int max1=Math.max(left+right+node.val,Math.max(left+node.val,right+node.val));
+        max=Math.max(max,Math.max(node.val,max1));
         
-        return Math.max(left,right)+node.val;
+        return Math.max(0,Math.max(left,right))+node.val;
     }
 }
