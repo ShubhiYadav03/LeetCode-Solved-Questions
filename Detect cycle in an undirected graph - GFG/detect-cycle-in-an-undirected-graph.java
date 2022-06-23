c88620d845ea53643e8fc47d2534bc956f682757
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-23 05:45:57 +0530

    Added solution - LeetHub

---
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(dfs(i,-1,visited,adj)) return true;
+            }
+        }
+        
+        return false;     // Code here
+    }
+    
+    boolean dfs(int v,int par, boolean[] visited, ArrayList<ArrayList<Integer>> adj){
+        //if(i>=V || visited[i]) return false;
+        visited[v] = true;
+       
+       for(int i:adj.get(v))
+       {
+          
+           if (!visited[i]) {
+               if (dfs(i, v , visited, adj)) return true;
+           }
+
+           else if (i != par)
+               return true;
+       }
+        
+        return false;
+    }
+}
