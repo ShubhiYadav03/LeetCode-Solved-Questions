787c8792db71a74ebcd734663e48f25d89a19086
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-06-26 05:20:40 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (11.29%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    TreeNode parent1=null, parent2=null;
+    int depth1=0, depth2=0;
+    public boolean isCousins(TreeNode root, int x, int y) {
+         if(root == null || x == root.val || y == root.val) return false;
+        cousins(root,x,y,0);
+        return (parent1!=parent2) && (depth1 == depth2);
+    }
+    
+    void cousins(TreeNode root, int x, int y, int depth){
+        if(root==null) return;
+        
+        if((root.left != null && root.left.val == x) || (root.right != null && root.right.val == x)){
+            parent1 = root;
+            depth1 = depth;
+        } 
+        if((root.left != null && root.left.val == y) || (root.right != null && root.right.val == y)){
+            parent2 = root;
+            depth2 = depth;
+        } 
+        
+        cousins(root.left,x,y,depth+1);
+        cousins(root.right,x,y,depth+1);
+        
+    }
+}
