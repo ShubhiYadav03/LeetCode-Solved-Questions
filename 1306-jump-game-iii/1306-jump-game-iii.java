8fd5279e879cbdcac6c26313210eabea82d7449b
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-03 23:28:26 +0530

    Time: 3 ms (98.82%), Space: 62.2 MB (35.01%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -5,13 +5,14 @@ class Solution {
     }
     
     boolean dfs(int start, boolean[] visited, int[] arr){
+        if(start < 0 || start >= arr.length) return false;
         if(arr[start] == 0)
 			return true;
         if(visited[start] || (arr[start]+start >= arr.length && start-arr[start] < 0)) return false;
 
         visited[start] = true;
-        if(arr[start]+start >= arr.length) return dfs(start-arr[start], visited, arr);
-        if(start-arr[start] < 0) return dfs(arr[start]+start, visited, arr);
+        // if(arr[start]+start >= arr.length) return dfs(start-arr[start], visited, arr);
+        // if(start-arr[start] < 0) return dfs(arr[start]+start, visited, arr);
         
          
         return dfs(arr[start]+start, visited, arr) || dfs(start-arr[start], visited, arr);
