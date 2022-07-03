c6c108b517556411e510ee5c98eedb9c1d7c2970
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-03 22:13:36 +0530

    Time: 111 ms (70.37%), Space: 97.3 MB (79.81%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,44 +1,44 @@
 class Solution {
     public int minJumps(int[] arr) {
-        if(arr.length==1) return 0;
-        HashMap<Integer,ArrayList<Integer>> hm=new HashMap<>();
-        boolean visited[]=new boolean[arr.length];
-        for(int i=0;i<arr.length;i++){
+        if(arr.length == 1) return 0;
+        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
+        boolean[] visited = new boolean[arr.length];
+        for(int i = 0; i < arr.length; i++){
             if(hm.containsKey(arr[i])) hm.get(arr[i]).add(i);
             else {
-                ArrayList<Integer> li=new ArrayList<>();
+                ArrayList<Integer> li = new ArrayList<>();
                 li.add(i);
-                hm.put(arr[i],li);
+                hm.put(arr[i], li);
             }
         }
-        int ans=0;
+        int ans = 0;
         boolean flag = false;
-        Queue<Integer> q=new ArrayDeque();
+        Queue<Integer> q = new ArrayDeque();
         q.add(0);
-        visited[0]=true;
+        visited[0] = true;
         while(!q.isEmpty()){
             ans++;
-            int len=q.size();
-            for(int i=0;i<len;i++){
-                int temp=q.poll();
-                if(temp+1==arr.length-1){
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+                int temp = q.poll();
+                if(temp + 1 == arr.length - 1){
                     return ans;
                 }
-                if(temp + 1 < arr.length && !visited[temp+1]){
-                    visited[temp+1]=true;
-                    q.add(temp+1); 
+                if(temp + 1 < arr.length && !visited[temp + 1]){
+                    visited[temp + 1] = true;
+                    q.add(temp + 1); 
                 }
                 if(temp - 1 > 0 && !visited[temp-1]){
-                    visited[temp-1]=true;
-                    q.add(temp-1); 
+                    visited[temp - 1] = true;
+                    q.add(temp - 1); 
                 }
                 if(hm.containsKey(arr[temp])){
-                for(int j=0;j<hm.get(arr[temp]).size();j++){
-                    int idx=hm.get(arr[temp]).get(j);
-                    if(idx==arr.length-1) return ans;
-                    if(idx!=temp && !visited[idx]){
+                for(int j = 0; j < hm.get(arr[temp]).size(); j++){
+                    int idx = hm.get(arr[temp]).get(j);
+                    if(idx == arr.length - 1) return ans;
+                    if(idx != temp && !visited[idx]){
                         q.add(idx);
-                        visited[idx]=true;
+                        visited[idx] = true;
                     }
                 }
                     hm.remove(arr[temp]);
