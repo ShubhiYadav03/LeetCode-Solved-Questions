d9004acb424d97a7fa60bcc658cbf119fedf0acc
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-15 01:51:38 +0530

    Time: 1 ms (97.98%), Space: 42 MB (62.53%) - LeetHub

--- 593-valid-square/593-valid-square.java
+++ 593-valid-square/593-valid-square.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        Set<Integer> set = new HashSet<>();
-        int[][] points = new int[][]{p1,p2,p3,p4};
-        for(int i = 0; i<points.length; i++){
-            for(int j = i+1; j<points.length; j++){
-                set.add(distance(points[i],points[j]));
-            }
+        double[] distances = new double[6];
+        distances[0] = Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2);
+        distances[1] = Math.pow(p2[0] - p3[0], 2) + Math.pow(p2[1] - p3[1], 2);
+        distances[2] = Math.pow(p3[0] - p4[0], 2) + Math.pow(p3[1] - p4[1], 2);
+        distances[3] = Math.pow(p4[0] - p1[0], 2) + Math.pow(p4[1] - p1[1], 2);
+        distances[4] = Math.pow(p4[0] - p2[0], 2) + Math.pow(p4[1] - p2[1], 2);
+        distances[5] = Math.pow(p3[0] - p1[0], 2) + Math.pow(p3[1] - p1[1], 2);
+        
+        double diagnal = 0, side = Integer.MAX_VALUE; 
+        int countDiag = 0, countSide = 0;
+        for(double distance : distances){
+            side = Math.min(distance, side);
+            diagnal = Math.max(distance, diagnal);
         }
-        return !set.contains(0) && set.size() == 2;
-    }
-    
-    public int distance(int[] p1, int[] p2){
-        return (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]);
+        if(side == 0) return false;
+        
+        for(double distance : distances){
+            if(distance == side) countSide++;
+            else if(distance == diagnal) countDiag++;
+        }
+        
+        return countSide == 4 && countDiag == 2;  
+        
     }
 }
