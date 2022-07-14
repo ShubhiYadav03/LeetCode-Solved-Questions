d683ac9dffac6353d1b868b8c6d9bbaebbec4a4d
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-14 18:49:45 +0530

    Time: 5 ms (40.08%), Space: 44.3 MB (59.12%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer,Integer> hm=new HashMap();
-        for(int i=0;i<inorder.length;i++){
-            hm.put(inorder[i],i);
+        HashMap<Integer, Integer> hm = new HashMap();
+        for(int i = 0; i < inorder.length; i++){
+            hm.put(inorder[i], i);
         }
-          return tree(0,preorder.length-1,preorder,0,inorder.length-1,inorder, hm);
+        return tree(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder, hm);
     }
-    TreeNode tree(int preStart,int preEnd,int[] preorder,int inStart,int inEnd, int[] inorder, HashMap<Integer,Integer> hm){
-        if(preStart>preEnd || inStart>inEnd) return null;
-        TreeNode root=new TreeNode(preorder[preStart]); 
-        int inRoot=hm.get(root.val);
-        int numsLeft=inRoot-inStart;
-        root.left=tree(preStart+1,preStart+numsLeft,preorder,inStart,inRoot-1,inorder,hm);
-        root.right=tree(preStart+numsLeft+1,preEnd,preorder,inRoot+1,inEnd,inorder,hm);
+    TreeNode tree(int preStart, int preEnd, int[] preorder, int inStart, int inEnd, int[] inorder, HashMap < Integer, Integer> hm){
+        if(preStart > preEnd || inStart > inEnd) return null;
+        TreeNode root = new TreeNode(preorder[preStart]); 
+        int inRoot = hm.get(root.val);
+        int numsLeft = inRoot - inStart;
+        root.left = tree(preStart + 1, preStart + numsLeft, preorder, inStart, inRoot - 1, inorder, hm);
+        root.right = tree(preStart + numsLeft + 1, preEnd, preorder, inRoot + 1, inEnd, inorder, hm);
         
         return root;
     }
