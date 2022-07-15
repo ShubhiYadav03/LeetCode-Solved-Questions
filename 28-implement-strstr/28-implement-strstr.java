6f73b986a3ecf61a4d94a44e426c4754eecbbbb3
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-16 00:52:39 +0530

    Time: 1 ms (64.96%), Space: 42.4 MB (15.01%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -4,6 +4,7 @@ class Solution {
         if(needle.length() > haystack.length()) return -1;
         
         for(int i = 0; i <= haystack.length() - needle.length(); i++){
+            if(haystack.charAt(i) != needle.charAt(0)) continue;
             if(haystack.substring(i, i + needle.length()).equals(needle)){
                 return i;
             }
