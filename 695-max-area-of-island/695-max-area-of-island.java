33f0cbd6e0798b696555975393a8c46e283da62e
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-15 21:13:50 +0530

    Time: 3 ms (84.14%), Space: 47.4 MB (42.79%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,29 +1,29 @@
 class Solution {
     
     public int maxAreaOfIsland(int[][] grid) {
-        int maxArea=0;
-        int area=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    area=dfs(grid,i,j);
-                    maxArea=Math.max(area,maxArea);
+        int maxArea = 0;
+        int area = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    area = dfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
                 }
             }
         }
         return maxArea;
     }
     
-    int dfs(int[][] grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0) return 0;
+    int dfs(int[][] grid, int i, int j){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return 0;
         
-        grid[i][j]=0;
+        grid[i][j] = 0;
         
-        int top=dfs(grid,i-1,j);
-        int bottom=dfs(grid,i+1,j);
-        int left=dfs(grid,i,j-1);
-        int right=dfs(grid,i,j+1);
+        int top = dfs(grid, i - 1, j);
+        int bottom = dfs(grid, i + 1, j);
+        int left = dfs(grid, i, j - 1);
+        int right = dfs(grid, i, j + 1);
         
-        return left+right+top+bottom+1;
+        return left + right + top + bottom + 1;
     }
 }
