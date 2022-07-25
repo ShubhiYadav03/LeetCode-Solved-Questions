af1cbc4890cb239b52eb607add407576b7c7f6f0
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-07-25 17:16:50 +0530

    Time: 10 ms (56.71%), Space: 45.2 MB (51.82%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.java
+++ 692-top-k-frequent-words/692-top-k-frequent-words.java
@@ -3,21 +3,16 @@ class Solution {
         Map<String, Integer> map = new HashMap();
         List<String> list = new ArrayList();
         for(String word : words){
-            if(map.containsKey(word)) map.put(word, map.get(word) + 1);
-            else map.put(word, 1);
+            map.put(word, map.getOrDefault(word, 0) + 1); 
         }
         
-        PriorityQueue<String> pq = new PriorityQueue<>(words.length, (a, b) -> map.get(b) == map.get(a) ? a.compareTo(b) : map.get(b) - map.get(a));
+//         PriorityQueue<String> pq = new PriorityQueue<>(words.length, (a, b) -> map.get(b) == map.get(a) ? a.compareTo(b) : map.get(b) - map.get(a));
         
         for (Map.Entry<String, Integer> it: map.entrySet()){
-            pq.add(it.getKey());
+            list.add(it.getKey());
         }
+        Collections.sort(list, (a, b) -> map.get(b) == map.get(a) ? a.compareTo(b) : map.get(b) - map.get(a));
         
-        while(!pq.isEmpty() && k > 0){
-            list.add(pq.poll());
-            k--;
-        }
-        
-        return list;
+        return list.subList(0, k);
     }
 }
