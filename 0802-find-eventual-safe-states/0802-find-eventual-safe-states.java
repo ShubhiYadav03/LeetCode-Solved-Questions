de2dcc0bf70d139e139df7ff051f93cd1696669d
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2022-12-20 20:44:02 +0530

    Time: 1944 ms (5.01%), Space: 50.8 MB (89.39%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        boolean[] visited = new boolean[graph.length];
+        boolean[] pathvis = new boolean[graph.length];
+        int[] safe = new int[graph.length];
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < graph.length; i++){
+            if(!visited[i]){
+                safeNodes(i, graph, pathvis, visited, safe);
+            }
+        }
+        
+        for(int i = 0; i < graph.length; i++){
+             if(safe[i] == 0) res.add(i);
+        }
+        
+        return res;
+    }
+
+    boolean safeNodes(int i, int[][] graph, boolean[] visited, boolean[] pathvis, int[] safe){
+        visited[i] = true;
+        pathvis[i] = true;
+
+        for(int n : graph[i]){
+            if(!visited[n]){
+                boolean cycle = safeNodes(n, graph, pathvis, visited, safe);
+                if(cycle){
+                    safe[i] = 1;
+                    return cycle;
+                }
+            }
+            else if(pathvis[n]){
+                safe[i] = 1;
+                return true;
+            }
+            else if(visited[n] && safe[n] == 1) safe[i] = 1;
+        }
+        pathvis[i] = false;
+        return false;
+    }
+}
