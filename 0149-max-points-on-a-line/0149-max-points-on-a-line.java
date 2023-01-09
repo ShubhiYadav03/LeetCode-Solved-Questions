32d3cf325f972ba96e554b0177d3e409ebf0db42
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-01-10 01:14:31 +0530

    Time: 13 ms (92.63%), Space: 42.4 MB (52.94%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,27 +1,32 @@
 class Solution {
     public int maxPoints(int[][] points) {
+        if(points.length == 1) return 1;
         int max = 0;
-        for(int[] point_1 : points)
-        {
-            Map<Double,Integer> map= new HashMap<>();
-            for(int[] point_2 : points)
-            {
-                if(point_1 == point_2) continue;
+        
+        for(int i = 0; i < points.length - 1; i++){
+            Map<Double, Integer> map = new HashMap();
+            for(int j = i + 1; j < points.length; j++){
+                Double slope = 0.0;
+                if(points[j][0] == points[i][0]) slope = Double.POSITIVE_INFINITY;
+                else if(points[j][1] == points[i][1]) slope = 0.0;
+                else slope = (double)(points[j][1] - points[i][1]) / (double)(points[j][0] - points[i][0]);
                 
-                double slope=0;
-                // if x2 - x1 are equal then line is vertical
-                if(point_2[0] == point_1[0])
-                   slope=Double.POSITIVE_INFINITY; 
-                else
-                {
-                    // slope (m) = (y2 - y1) / (x2 - x1) 
-                    slope = (point_2[1] - point_1[1]) / (double)(point_2[0] - point_1[0]);
+                if(map.containsKey(slope)){
+                    map.put(slope, map.get(slope) + 1);
+                }else{
+                    map.put(slope, 2);
                 }
-                map.put( slope , map.getOrDefault(slope,0)+1 );
+                
                 if(map.get(slope)>max)
                     max=map.get(slope);
             }
+            // int max = 0;
+            // for(double key : map.keySet()){
+            //     if(map.get(key) > max) max = map.get(key);
         }
-        return max+1;
+            
+            // if(max > ans) ans = max;
+            
+         return max;
     }
 }
