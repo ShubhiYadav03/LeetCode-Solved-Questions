00bd70e59323b2a4b467fe4b1bbeb8cace439d05
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-01-14 01:23:33 +0530

    Time: 34 ms (62.63%), Space: 85.3 MB (59.16%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -0,0 +1,38 @@
+class Solution {
+    int time = 0;
+    public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int [] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        
+        dfs(0, -1, adj, hasApple);  
+        return time;
+    }
+    
+    boolean dfs(int i, int par, List<List<Integer>> adj, List<Boolean> hasApple){
+        boolean apple = false;
+        for(int n : adj.get(i)){
+            if(n != par){
+                time++;
+                boolean b = dfs(n, i, adj, hasApple);
+                apple = apple | b;
+                if(!b) time--;
+                else time++;
+                //return apple;
+            }
+        }
+        
+        if(hasApple.get(i)){
+            //time++;
+            return true;
+        } 
+       // time = (apple) ? time + 1 : time - 1;
+        return apple;
+    }
+}
