3533500625abd20b9c89392a31102c498a653ca3
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-02-08 00:29:23 +0530

    Time: 199 ms (62.09%), Space: 40.5 MB (67.35%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,15 +1,14 @@
 class Solution {
-    boolean ifexist = false;
+    // boolean ifexist = false;
     public boolean exist(char[][] board, String word) {
        if(word.length() > board.length * board[0].length) return false;
         StringBuilder sb = new StringBuilder();
-        boolean[][] visited = new boolean[board.length][board[0].length];
+        
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(word.charAt(0) == board[i][j]){
-                    
-                    dfs(i, j, 0, board, word, visited);
-                    if(ifexist) return true;
+                    boolean[][] visited = new boolean[board.length][board[0].length];
+                    if(dfs(i, j, 0, board, word, visited)) return true;
                 }
             }
         }
@@ -19,19 +18,19 @@ class Solution {
         return false;
     }
     
-    void dfs(int i, int j, int ch, char[][] board, String word, boolean[][] visited){
+    boolean dfs(int i, int j, int ch, char[][] board, String word, boolean[][] visited){
         if(ch == word.length()){
-            ifexist = true; 
-            return;
+            return true;
         }
-        if(i < 0  || j < 0 || i >= board.length || j >= board[0].length || ifexist || visited[i][j]) return;
+        if(i < 0  || j < 0 || i >= visited.length || j >= visited[0].length || visited[i][j] || (word.charAt(ch) != board[i][j]))
+            return false;
+        boolean res = false;
         visited[i][j] = true;
-        if(word.charAt(ch) == board[i][j]){
-            dfs(i - 1, j, ch + 1, board, word, visited);
-            dfs(i + 1, j, ch + 1, board, word, visited);
-            dfs(i, j - 1, ch + 1, board, word, visited);
-            dfs(i, j + 1, ch + 1, board, word, visited);
-        }
+        res = res | dfs(i - 1, j, ch + 1, board, word, visited);
+        res = res | dfs(i + 1, j, ch + 1, board, word, visited);
+        res = res | dfs(i, j - 1, ch + 1, board, word, visited);
+        res = res | dfs(i, j + 1, ch + 1, board, word, visited);
         visited[i][j] = false;
+        return res;
     }
 }
