55ef3b9a0bfe1f56915200af5a4121961f89d325
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-02-08 00:35:03 +0530

    Time: 1 ms (93.16%), Space: 42.7 MB (35.75%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList();
+    
+        parentheses(n, 0, 0, list, "");
+        
+        return list;
+    }
+    
+    void parentheses(int n, int open, int close, List<String> list, String s){
+        if(s.length() == n * 2){
+            list.add(s);
+            return;
+        }
+        if(open < n)
+            parentheses(n, open + 1, close, list, s + "(");
+        if(close < open)
+            parentheses(n, open, close + 1, list, s + ")");
+    }
+}
