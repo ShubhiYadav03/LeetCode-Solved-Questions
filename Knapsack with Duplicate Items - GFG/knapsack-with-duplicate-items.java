f469b030e2e629056a415f77c14a25c6fef28e21
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-03-24 01:39:27 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -35,25 +35,29 @@ class GFG{
 class Solution{
     static int knapSack(int n, int W, int val[], int wt[])
     {
-        int[][] dp = new int[n][W + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return solve(0, W, wt, val, n, dp);
+        // int[][] dp = new int[n][W + 1];
+        // for(int[] row : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        // return solve(0, W, wt, val, n, dp);
+        
+        return tabulation(W, wt, val, n);
     } 
     
-    static int tabulatin(int W, int wt[], int val[], int n){
+    static int tabulation(int W, int wt[], int val[], int n){
         int[][] dp = new int[n + 1][W + 1];
-        
+       
         for(int idx = n - 1; idx >= 0; idx--){
             for(int cap = 0; cap <= W; cap++){
-                // if(idx == n || cap == 0) dp[n][cap] = 0;
+                // if(idx == n || cap == 0) dp[idx][cap] = 0;
+                // else{
                 int pick = 0;
                 if(cap >= wt[idx])
                     pick = dp[idx][cap - wt[idx]] + val[idx];
         
                 int notpick = dp[idx + 1][cap];  
                 dp[idx][cap] = Math.max(pick, notpick);
+            // }
             }
         }
         
