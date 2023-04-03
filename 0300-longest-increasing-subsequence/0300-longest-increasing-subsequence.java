06cb83a03aab41a306ecf67511ad0a9618f07d5d
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-04-04 00:32:20 +0530

    Time: 134 ms (14.62%), Space: 109.4 MB (11.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,25 +1,40 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n];
-        for(int[] row : dp) 
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n + 2][n + 1];
+//         for(int[] row : dp) 
+//             Arrays.fill(row, -1);
         
-        return lis(-1, 0, nums, dp);
-    }
-    
-    int lis(int idx1, int idx2, int[] nums, int[][] dp){
-        int n = nums.length;
-        if(idx2 == n || idx1 == n) return 0;
+//         return lis(-1, 0, nums, dp);
         
-        if(dp[idx1 + 1][idx2] != -1) return dp[idx1 + 1][idx2];
-        int pick = 0;
-        if(idx1 == -1 || nums[idx2] > nums[idx1]){
-            pick = lis(idx2, idx2 + 1, nums, dp) + 1;
+        
+        for(int curr = n - 1; curr >= 0; curr--){
+            for(int prev = curr - 1; prev >= -1; prev--){
+                int pick = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    pick = dp[curr + 1][curr + 1] + 1;
+                }
+
+                int notpick = dp[curr + 1][prev + 1];
+                dp[curr][prev + 1] = Math.max(pick, notpick);
+            }
         }
         
-        int notpick = lis(idx1, idx2 + 1, nums, dp);
-            
-        return dp[idx1 + 1][idx2] = Math.max(pick, notpick);
+        return dp[0][0];
     }
+    
+//     int lis(int idx1, int idx2, int[] nums, int[][] dp){
+//         int n = nums.length;
+//         if(idx2 == n || idx1 == n) return 0;
+        
+//         if(dp[idx1 + 1][idx2] != -1) return dp[idx1 + 1][idx2];
+//         int pick = 0;
+//         if(idx1 == -1 || nums[idx2] > nums[idx1]){
+//             pick = lis(idx2, idx2 + 1, nums, dp) + 1;
+//         }
+        
+//         int notpick = lis(idx1, idx2 + 1, nums, dp);
+            
+//         return dp[idx1 + 1][idx2] = Math.max(pick, notpick);
+//     }
 }
