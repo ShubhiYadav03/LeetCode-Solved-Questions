a349cef25927d93f377cfc477d3952b650bf0371
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-07-19 23:50:19 +0530

    Time: 12 ms (37.36%), Space: 55.6 MB (39.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,34 +1,33 @@
 class Solution {
-    int max = 0;
+    int landcells = 0;
     public int numEnclaves(int[][] grid) {
-        int enclaves = 0;
-
+        int numEnclaves = 0;
+        
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == 1){
-                    if(dfs(grid, i, j)){
-                        enclaves += max;
-                    }
-                    max = 0;
+                    if(dfs(i, j, grid))
+                        numEnclaves += landcells;
+                    landcells = 0;
                 }
             }
         }
-        return enclaves;
+        
+        return numEnclaves;
     }
     
-    boolean dfs(int[][] grid, int i, int j){
+    boolean dfs(int i, int j, int[][] grid){
         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return false;
         if(grid[i][j] == 0) return true;
+        
         grid[i][j] = 0;
+        landcells += 1;
         
+        boolean top = dfs(i - 1, j, grid);
+        boolean bottom = dfs(i + 1, j, grid);
+        boolean left = dfs(i, j - 1, grid);
+        boolean right = dfs(i, j + 1, grid); 
         
-        boolean top = dfs(grid, i - 1, j);
-        boolean bottom = dfs(grid, i + 1, j);
-        boolean left = dfs(grid, i, j - 1);
-        boolean right = dfs(grid, i, j + 1);
-        
-        if(left && right && top && bottom) max += 1;
-
-        return left && right && top && bottom;
+        return top && bottom && left && right;
     }
 }
