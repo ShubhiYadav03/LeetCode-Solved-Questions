b2553f7a8caf65ff4b87c812e021350db0a31f73
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-07-25 19:28:43 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        List<List<Integer>> adj = new ArrayList();
+        
+        for(int i = 0; i < n ; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        int[] distances = new int[n];
+        Arrays.fill(distances, -1);
+        //boolean[] visited = new boolean[n];
+        //visited[src] = true;
+        distances[src] = 0;
+        Queue<Integer> q = new ArrayDeque();
+        q.add(src);
+        while(!q.isEmpty()){
+            int node = q.remove();
+            
+            for(int next : adj.get(node)){
+                if(distances[next] == -1){
+                   // visited[next] = true;
+                    distances[next] = distances[node] + 1;
+                    q.add(next);
+                }
+            }
+        }
+        
+        return distances;
+    }
+}
