58dd5e3cf07ed910dde0cb8d33c70f7b41b46857
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-07-28 13:45:37 +0530

    Time: 16 ms (63.35%), Space: 44.2 MB (98.55%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,11 +1,9 @@
 class Pair{
     int row;
     int col;
-    int steps;
-    Pair(int r, int c, int s){
+    Pair(int r, int c){
         row = r;
         col = c;
-        steps = s;
     }
 }
 class Solution {
@@ -23,13 +21,13 @@ class Solution {
             }
         }
     
-        q.add(new Pair(0, 0, 1));
+        q.add(new Pair(0, 0));
         distances[0][0] = 1;
         while(!q.isEmpty()){
             Pair p = q.remove();
             int row = p.row;
             int col = p.col;
-            int steps = p.steps;
+            int steps = distances[row][col];
             
             for(int dy : diry){
                 for(int dx : dirx){
@@ -38,7 +36,7 @@ class Solution {
                     if(r == n - 1 && c == n - 1) return steps + 1;
                     if(isSafe(r, c, n) && grid[r][c] == 0 && distances[r][c] > steps + 1){
                         distances[r][c] = steps + 1;
-                        q.add(new Pair(r, c, steps + 1));
+                        q.add(new Pair(r, c));
                     }
                 }
             }
