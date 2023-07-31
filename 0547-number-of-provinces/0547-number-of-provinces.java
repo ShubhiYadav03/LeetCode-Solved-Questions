64eebeb34f1469b6dabae4ddf2fe9e3e2e8245c4
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-01 02:05:45 +0530

    Time: 2 ms (41.25%), Space: 45.8 MB (28.66%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,40 +1,59 @@
+class DisjointSet{
+    List<Integer> parent = new ArrayList();
+    List<Integer> size = new ArrayList();  
+    
+    DisjointSet(int n){
+        for(int i = 0; i < n; i++){
+            parent.add(i);
+            size.add(1);
+        }
+    }
+    
+    int findUlPar(int node){
+        if(parent.get(node) == node)
+            return node;
+        int ulp = findUlPar(parent.get(node));
+        parent.set(node, ulp);
+        return ulp;
+    }
+    
+    void unionBySize(int u, int v){
+        int ulpu = findUlPar(u);
+        int ulpv = findUlPar(v);
+        
+        if(ulpu < ulpv){
+            parent.set(ulpu, ulpv);
+            size.set(ulpv, size.get(ulpu) + size.get(ulpv));
+        }
+        else{
+            parent.set(ulpv, ulpu);
+            size.set(ulpu, size.get(ulpu) + size.get(ulpv));
+        }
+    }
+}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         if(n == 1) return 1;
         
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<Integer>());
-        }
+        DisjointSet ds = new DisjointSet(n);
         
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < isConnected[0].length; j++){
                 if(isConnected[i][j] == 1){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+                    ds.unionBySize(i, j);
                 }
             }
         }
         
         int provinces = 0;
-        boolean[] visited = new boolean[n];
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(i, visited, adj);
+        
+        for(int i = 0; i < ds.parent.size(); i++){
+            if(i == ds.parent.get(i)){
                 provinces++;
             }
         }
         
         return provinces;
-    }
-    
-    void dfs(int i, boolean[] visited, List<List<Integer>> adj){
-        visited[i] =true;
-        
-        for(int n : adj.get(i)){
-            if(!visited[n])
-                dfs(n, visited, adj);
-        }
     }
 }
