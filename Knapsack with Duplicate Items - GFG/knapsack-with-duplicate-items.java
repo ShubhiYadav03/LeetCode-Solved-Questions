a456484b44e489d4c87b5f0b910971154e272fc9
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-04 16:34:52 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -35,51 +35,26 @@ class GFG{
 class Solution{
     static int knapSack(int n, int W, int val[], int wt[])
     {
-        // int[][] dp = new int[n][W + 1];
-        // for(int[] row : dp){
-        //     Arrays.fill(row, -1);
-        // }
-        // return solve(0, W, wt, val, n, dp);
-        
-        return tabulation(W, wt, val, n);
-    } 
-    
-    static int tabulation(int W, int wt[], int val[], int n){
-        int[][] dp = new int[n + 1][W + 1];
-       
-        for(int idx = n - 1; idx >= 0; idx--){
-            for(int cap = 0; cap <= W; cap++){
-                // if(idx == n || cap == 0) dp[idx][cap] = 0;
-                // else{
-                int pick = 0;
-                if(cap >= wt[idx])
-                    pick = dp[idx][cap - wt[idx]] + val[idx];
-        
-                int notpick = dp[idx + 1][cap];  
-                dp[idx][cap] = Math.max(pick, notpick);
-            // }
-            }
+        int dp[][] = new int[n][W + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
         
-        return dp[0][W];
+        return solve(0, 0, wt, val, W, dp);
     }
     
-    static int solve(int idx, int cap, int wt[], int val[], int n, int[][] dp){
-        if(idx == n || cap == 0){
+    static int solve(int idx, int currWt, int wt[], int val[], int totalWt, int dp[][]){
+        int n = wt.length;
+        if(idx == n){
             return 0;
         }
-        
-        if(dp[idx][cap] != -1){
-            return dp[idx][cap];
-        }
-        
+        if(dp[idx][currWt] != -1) return dp[idx][currWt];
         int pick = 0;
-        if(cap >= wt[idx]){
-            pick = solve(idx, cap - wt[idx], wt, val, n, dp) + val[idx];
+        if(wt[idx] + currWt <= totalWt){
+            pick = val[idx] + solve(idx, currWt + wt[idx], wt, val, totalWt, dp);
         }
+        int notpick = solve(idx + 1, currWt, wt, val, totalWt, dp);
         
-        int notpick = solve(idx + 1, cap, wt, val, n, dp); 
-        
-        return dp[idx][cap] = Math.max(pick, notpick);
+        return dp[idx][currWt] = Math.max(pick, notpick);
     }
 }
