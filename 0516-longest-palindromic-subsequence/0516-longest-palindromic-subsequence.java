ffa41783b18e03dbeefbda51552234686cca9490
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-06 23:38:41 +0530

    Time: 28 ms (96.59%), Space: 54.9 MB (14.61%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,42 +1,32 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         int n = s.length();
-        // int[][] dp = new int[n][n];
-        // for(int[] row : dp){
-        //     Arrays.fill(row, -1);
-        // }
-        // return solve(0, n - 1, s, dp);
-        
-        return tabulation(s, n);
-    }
-    
-    int tabulation(String s, int n){
-        int[][] dp = new int[n + 1][n + 1];
-        
-        for(int idx1 = n - 1; idx1 >= 0; idx1--){
-            for(int idx2 = 1; idx2 < n + 1; idx2++){
-                if(s.charAt(idx1) == s.charAt(idx2 - 1))
-                    dp[idx1][idx2] = dp[idx1 + 1][idx2 - 1] + 1;
-                else 
-                    dp[idx1][idx2] = Math.max(dp[idx1 + 1][idx2], dp[idx1][idx2 - 1]);
-            }
+        int dp[][] = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
         
-        return dp[0][n];
+        return solve(0, n - 1, s, dp);
     }
-    
-    int solve(int idx1, int idx2, String s, int[][] dp){
-        int n = s.length();
-        if(idx1 == n || idx2 < 0){
+    int solve(int idx1, int idx2, String str, int[][] dp){
+        int n = str.length();
+        if(idx1 > idx2){
             return 0;
         }
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
-        
-        if(s.charAt(idx1) == s.charAt(idx2)){
-            return dp[idx1][idx2] = solve(idx1 + 1, idx2 - 1, s, dp) + 1;
+        if(idx1 == idx2){
+            return 1;
         }
         
-        return dp[idx1][idx2] = Math.max(solve(idx1 + 1, idx2, s, dp), solve(idx1, idx2 - 1, s, dp));
-             
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
+        int pick = 0;
+        if(str.charAt(idx1) == str.charAt(idx2)){
+            pick = 2 + solve(idx1 + 1, idx2 - 1, str, dp);
+            return dp[idx1][idx2] = pick;
+        }
+        
+        int notpick1 = solve(idx1, idx2 - 1, str, dp);
+        int notpick2 = solve(idx1 + 1, idx2, str, dp);
+        
+        return dp[idx1][idx2] = Math.max(pick, Math.max(notpick1, notpick2));
     }
 }
