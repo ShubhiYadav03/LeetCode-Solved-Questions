b8ab6f17c4ee01a9eede8b438c61450cd2713385
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-11 23:32:52 +0530

    Time: 8 ms (45.06%), Space: 45.8 MB (44.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,28 +2,19 @@ class Solution {
     public int change(int amount, int[] coins) {
         if(amount == 0) return 1;
     
-        int[][] dp = new int[coins.length][amount + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[coins.length + 1][amount + 2];
+        dp[coins.length][amount] = 1;
+        for(int idx = coins.length - 1; idx >= 0; idx--){
+            for(int sum = amount; sum >= 0; sum--){
+                int pick = 0;
+                if(coins[idx] + sum <= amount && sum + coins[idx] >= 0){
+                    pick = dp[idx][sum + coins[idx]];
+                }
+                int notpick = dp[idx + 1][sum];
+
+                dp[idx][sum] = pick + notpick;
+            }
         }
-     
-        return solve(0, 0, coins, amount, dp);
-    }
-    
-    int solve(int idx, int sum, int[] coins, int amount, int dp[][]){
-        int n = coins.length;
-        if(idx == n && sum == amount){
-            return 1;
-        }
-        if(idx == n) return 0;
-        
-        if(dp[idx][sum] != -1) return dp[idx][sum];
-        int pick = 0;
-        if(coins[idx] + sum <= amount && sum + coins[idx] >= 0){
-            pick = solve(idx, sum + coins[idx], coins, amount, dp);
-        }
-        int notpick = solve(idx + 1, sum, coins, amount, dp);
-        
-        return dp[idx][sum] = pick + notpick;
+        return dp[0][0];
     }
 }
