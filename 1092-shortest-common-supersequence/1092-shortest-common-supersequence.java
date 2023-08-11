5942a29b030b7073f9cecc21748346d238bbf636
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-11 22:23:51 +0530

    Time: 32 ms (8.41%), Space: 48.4 MB (46.17%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,54 +1,50 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        int m = str1.length(), n = str2.length();
-        int[][] dp = new int[m + 1][n + 1];
+    public String shortestCommonSupersequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        int dp[][] = new int[m + 1][n + 1];
         
         for(int idx1 = m - 1; idx1 >= 0; idx1--){
             for(int idx2 = n - 1; idx2 >= 0; idx2--){
-                int equal = 0;
-                if(str1.charAt(idx1) == str2.charAt(idx2))
-                    equal = dp[idx1 + 1][idx2 + 1] + 1;
-        
-                int pick1 = dp[idx1][idx2 + 1];
-                int pick2 = dp[idx1 + 1][idx2];
-        
-                dp[idx1][idx2] = Math.max(equal, Math.max(pick1, pick2));
+                char ch1 = text1.charAt(idx1), ch2 = text2.charAt(idx2);
+                int pick = 0;
+                if(ch1 == ch2)
+                    pick = dp[idx1 + 1][idx2 + 1] + 1;
+            
+                int notpick1 = dp[idx1 + 1][idx2];
+                int notpick2 = dp[idx1][idx2 + 1];
+
+                dp[idx1][idx2] = Math.max(pick, Math.max(notpick1, notpick2));
             }
         }
         
-        System.out.println(Arrays.deepToString(dp));
         String ans = "";
+        
         int i = 0, j = 0;
-        while(i < m &&  j < n){
-            if(str1.charAt(i) == str2.charAt(j)){
-                ans += str1.charAt(i);
+        
+        while(i < m && j < n){
+            if(text1.charAt(i) == text2.charAt(j)){
+                ans += text1.charAt(i);
                 i++;
                 j++;
+                continue;
+            }
+            if(dp[i + 1][j] >= dp[i][j + 1]){
+                ans += text1.charAt(i);
+                i++;
             }else{
-                if(dp[i + 1][j] >= dp[i][j + 1]){
-                    ans += str1.charAt(i);
-                    i++;
-                }else if(dp[i + 1][j] < dp[i][j + 1]){
-                    ans += str2.charAt(j);
-                    j++;
-                }
-                    // else{
-                //     // ans += str1.charAt(i);
-                //     // ans += str2.charAt(j);
-                //     // i++;
-                // }
+                ans += text2.charAt(j);
+                j++;
             }
         }
         
         while(i < m){
-            ans += str1.charAt(i++);   
+            ans += text1.charAt(i++);   
         }
         
         while(j < n){
-            ans += str2.charAt(j++);   
+            ans += text2.charAt(j++);   
         }
         
         return ans;
     }
-    
 }
