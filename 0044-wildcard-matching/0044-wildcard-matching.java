b47f6aca0bc3e96537ba8bd90bdb30a5002890e3
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-12 18:35:19 +0530

    Time: 17 ms (84.28%), Space: 47.3 MB (17.72%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,47 +1,38 @@
 class Solution {
     public boolean isMatch(String s, String p) {
         if(s.equals(p)) return true;
-        if(p.length() == 1 && p.charAt(0) == '*') return true;
         int m = s.length(), n = p.length();
         int[][] dp = new int[m][n];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
         
-        return match(0, 0, s, p, dp);
+        int ans = solve(0, 0, s, p, dp);
+        return ans == 1;
     }
     
-    boolean match(int idx1, int idx2, String s, String p, int[][] dp){
+    int solve(int idx1, int idx2, String s, String p, int[][] dp){
         int m = s.length(), n = p.length();
-        if(idx1 == m && idx2 == n) return true;
-        if(idx2 == n) return false;
+        if(idx1 == m && idx2 == n) return 1;
+        if(idx2 == n) return -1;
         if(idx1 == m){
-            if(p.charAt(idx2) == '*'){
-                while(idx2 < n && p.charAt(idx2) == '*'){
-                    idx2++;
-                }
-                
-                return idx2 == n;
+            while(idx2 < n && p.charAt(idx2) == '*'){
+                idx2++;
             }
-            return false;
-        }
-        
-        if(dp[idx1][idx2] != -1) return dp[idx1][idx2] == 1;
-        
-         // pick = false, notpick = false;
-        if(s.charAt(idx1) == p.charAt(idx2) || p.charAt(idx2) == '?'){
-            boolean pick = match(idx1 + 1, idx2 + 1, s, p, dp);
-            dp[idx1][idx2] = (pick) ? 1 : 0;
-            return pick;
-        }
-        else if(s.charAt(idx1) != p.charAt(idx2) && p.charAt(idx2) == '*'){
             
-            boolean notpick = match(idx1 + 1, idx2, s, p, dp) || match(idx1 + 1, idx2 + 1, s, p, dp) || match(idx1, idx2 + 1, s, p, dp);
-            dp[idx1][idx2] = (notpick) ? 1 : 0;
-            return notpick;
+            return (idx2 == n) ? 1 : -1;
         }
-        dp[idx1][idx2] = 0;
-        return false;
+        
+        if(dp[idx1][idx2] != 0) return dp[idx1][idx2];
+        
+        char chs = s.charAt(idx1), chp = p.charAt(idx2);
+        if(chs == chp || chp == '?'){
+            return dp[idx1][idx2] = solve(idx1 + 1, idx2 + 1, s, p, dp);
+        }
+        
+        if(chp == '*'){
+            int pick = solve(idx1 + 1, idx2, s, p, dp);
+            int notpick = solve(idx1, idx2 + 1, s, p, dp);
+            
+            return dp[idx1][idx2] = (pick == 1) ? pick : notpick;
+        }
+        return -1;
     }
-    
 }
