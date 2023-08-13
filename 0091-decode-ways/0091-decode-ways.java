2588bbb0c557740b920b1abb179e3cce8d9f65b8
Shubhi Yadav <77799883+ShubhiYadav03@users.noreply.github.com>
2023-08-13 19:40:59 +0530

    Time: 58 ms (5.12%), Space: 44.3 MB (5.33%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,39 +1,29 @@
 class Solution {
     public int numDecodings(String s) {
-        if(s.charAt(0) == '0') return 0;
         int n = s.length();
-        int[] dp = new int[n + 1];
-        // Arrays.fill(dp, -1);
-        // return ways(n - 1, s, dp);
-        dp[0] = 1;
-        for(int idx = 1; idx <= n; idx++){
-            int pick = 0;
-            if(s.charAt(idx - 1) != '0')
-                pick = dp[idx - 1];
-            int pick2 = 0;
-            if(idx > 1 && s.charAt(idx - 2) != '0' && Integer.parseInt(s.charAt(idx - 2) +""+ s.charAt(idx - 1)) <= 26){
-                pick2 = dp[idx - 2];
-            }
-            
-            dp[idx] = pick + pick2;
+        Set<String> set = new HashSet<>();
+        int[] dp = new int[n];
+   
+        for(int i = 1; i <= 26; i++){
+            set.add(i + "");
         }
-        return dp[n];
+        
+        return solve(0, s, set, dp);
     }
     
-//     int ways(int idx, String s, int[] dp){
-//         int n = s.length();
-//         if(idx < 0) return 1;
+    int solve(int idx, String s, Set<String> set, int[] dp){
+        int n = s.length();
+        if(idx == n) return 1;
         
-//         if(dp[idx] != -1) return dp[idx];
-//         int pick = 0;
-//         if(s.charAt(idx) != '0')
-//             pick = ways(idx - 1, s, dp);
-//         int pick2 = 0;
-//         if(idx > 0 && s.charAt(idx - 1) != '0' && Integer.parseInt(s.charAt(idx - 1) +""+ s.charAt(idx)) <= 26){
-//             pick2 = ways(idx - 2, s, dp);
-//         }
+        if(dp[idx] != 0) return dp[idx];
+        int pick1 = 0;
+        if(s.charAt(idx) != '0') pick1 = solve(idx + 1, s, set, dp);
         
-//         return dp[idx] = pick + pick2;
-//     }
-    
+        int pick2 = 0;
+        if(idx + 1 < n && set.contains(s.substring(idx, idx + 2))){
+            pick2 = solve(idx + 2, s, set, dp);
+        }
+        
+        return dp[idx] = pick1 + pick2;
+    }
 }
